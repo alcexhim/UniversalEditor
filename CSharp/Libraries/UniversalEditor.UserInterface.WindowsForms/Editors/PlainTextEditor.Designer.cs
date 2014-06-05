87cc69d8fc6e8175b5b70edf004abfeb3cadf163
alcexhim <alcexhim@gmail.com>
2014-06-04 20:19:39 -0400

    Use WinForms text editor until we can figure out why AwesomeControls won't play well with huge text files

--- CSharp/Libraries/UniversalEditor.UserInterface.WindowsForms/Editors/PlainTextEditor.Designer.cs
+++ CSharp/Libraries/UniversalEditor.UserInterface.WindowsForms/Editors/PlainTextEditor.Designer.cs
@@ -28,41 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txt = new AwesomeControls.TextBox.TextBoxControl();
+            this.txt = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // txt
             // 
-            this.txt.AcceptReturn = true;
-            this.txt.AutoSuggestFilter = true;
-            this.txt.AutoSuggestMode = AwesomeControls.TextBox.TextBoxAutoSuggestMode.None;
             this.txt.BackColor = System.Drawing.SystemColors.Window;
-            this.txt.CaretBlinkInterval = 530;
-            this.txt.CaretColor = System.Drawing.Color.Black;
-            this.txt.CaretOrientation = System.Windows.Forms.Orientation.Vertical;
-            this.txt.CaretSize = 1;
-            this.txt.CharacterSpacing = 0;
             this.txt.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txt.EnableCaret = true;
-            this.txt.EnableCaretBlink = true;
-            this.txt.EnableOutlining = false;
-            this.txt.EnableOverwrite = false;
-            this.txt.EnableOverwriteShortcut = true;
-            this.txt.EnableSelection = true;
-            this.txt.EnableSyntaxHighlight = false;
             this.txt.HideSelection = false;
-            this.txt.LineSeparator = AwesomeControls.TextBox.TextBoxLineSeparator.Default;
-            this.txt.LineSeparatorString = "\r\n";
             this.txt.Location = new System.Drawing.Point(0, 0);
             this.txt.Multiline = true;
             this.txt.Name = "txt";
-            this.txt.PlaceholderText = "";
-            this.txt.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Both;
             this.txt.SelectionStart = 0;
             this.txt.Size = new System.Drawing.Size(533, 322);
-            this.txt.CaseSensitive = true;
             this.txt.TabIndex = 0;
-            this.txt.WordSpacing = 0;
             this.txt.TextChanged += new System.EventHandler(this.txt_TextChanged);
             this.txt.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_KeyDown);
             // 
@@ -79,6 +58,7 @@
 
         #endregion
 
-        private AwesomeControls.TextBox.TextBoxControl txt;
+        // private AwesomeControls.TextBox.TextBoxControl txt;
+        private System.Windows.Forms.TextBox txt;
     }
 }
