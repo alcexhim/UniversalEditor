faee706cc9515402dc987fe2d2f9bb06961726a9
alcexhim <alcexhim@gmail.com>
2014-06-09 16:07:39 -0400

    Add DataFormats for Microsoft Visual Studio solution and prjoect files

---
+++ CSharp/Plugins/UniversalEditor.Essential/DataFormats/Solution/Microsoft/VisualStudio/VisualStudioProjectDataFormat.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UniversalEditor.ObjectModels.Markup;
+using UniversalEditor.DataFormats.Markup.XML;
+
+using UniversalEditor.ObjectModels.Solution;
+
+namespace UniversalEditor.DataFormats.Solution.Microsoft.VisualStudio
+{
+	public class VisualStudioProjectDataFormat : XMLDataFormat
+	{
+		private static DataFormatReference _dfr = null;
+		public override DataFormatReference MakeReference()
+		{
+			if (_dfr == null)
+			{
+				_dfr = base.MakeReference();
+				_dfr.Capabilities.Add(typeof(SolutionObjectModel), DataFormatCapabilities.All);
+				_dfr.Filters.Add("Microsoft Visual Studio project", new string[] { "*.vbproj", "*.csproj", "*.jsproj", "*.wixproj", "*.fsproj", "*.pyproj", "*.sqlproj", "*.phpproj" });
+			}
+			return _dfr;
+		}
+
+		protected override void BeforeLoadInternal(Stack<ObjectModel> objectModels)
+		{
+			objectModels.Push(new MarkupObjectModel());
+		}
+		protected override void AfterLoadInternal(Stack<ObjectModel> objectModels)
+		{
+			MarkupObjectModel mom = (objectModels.Pop() as MarkupObjectModel);
+			SolutionObjectModel sln = (objectModels.Pop() as SolutionObjectModel);
+		}
+		protected override void BeforeSaveInternal(Stack<ObjectModel> objectModels)
+		{
+			SolutionObjectModel sln = (objectModels.Pop() as SolutionObjectModel);
+			MarkupObjectModel mom = new MarkupObjectModel();
+
+			Project proj = sln.Projects[0];
+
+			MarkupTagElement tagProject = new MarkupTagElement();
+			tagProject.FullName = "Project";
+			// remove for compatibility with older VS versions
+			tagProject.Attributes.Add("ToolsVersion", "4.0");
+			tagProject.Attributes.Add("DefaultTargets", "Build");
+			tagProject.Attributes.Add("xmlns", "http://schemas.microsoft.com/developer/msbuild/2003");
+
+			#region PropertyGroup
+			{
+				MarkupTagElement tagPropertyGroup = new MarkupTagElement();
+				tagPropertyGroup.FullName = "PropertyGroup";
+
+				MarkupTagElement tagConfiguration = new MarkupTagElement();
+				tagConfiguration.FullName = "Configuration";
+				tagConfiguration.Attributes.Add("Condition", " '$(Configuration)' == '' ");
+				tagConfiguration.Value = "Debug";
+				tagPropertyGroup.Elements.Add(tagConfiguration);
+
+				MarkupTagElement tagName = new MarkupTagElement();
+				tagName.FullName = "Name";
+				tagName.Value = proj.Title;
+				tagPropertyGroup.Elements.Add(tagName);
+
+				MarkupTagElement tagProjectGUID = new MarkupTagElement();
+				tagProjectGUID.FullName = "ProjectGuid";
+				tagProjectGUID.Value = proj.ID.ToString("D");
+				tagPropertyGroup.Elements.Add(tagProjectGUID);
+
+				MarkupTagElement tagOutputType = new MarkupTagElement();
+				tagOutputType.FullName = "OutputType";
+				tagOutputType.Value = "Library";
+				tagPropertyGroup.Elements.Add(tagOutputType);
+
+				MarkupTagElement tagRootNamespace = new MarkupTagElement();
+				tagRootNamespace.FullName = "RootNamespace";
+				tagRootNamespace.Value = proj.Title;
+				tagPropertyGroup.Elements.Add(tagRootNamespace);
+
+				MarkupTagElement tagProjectTypeGUIDs = new MarkupTagElement();
+				tagProjectTypeGUIDs.FullName = "ProjectTypeGuids";
+				tagProjectTypeGUIDs.Value = proj.ProjectType.ID.ToString("B");
+				tagPropertyGroup.Elements.Add(tagProjectTypeGUIDs);
+
+				MarkupTagElement tagAssemblyName = new MarkupTagElement();
+				tagAssemblyName.FullName = "AssemblyName";
+				tagAssemblyName.Value = proj.Title;
+				tagPropertyGroup.Elements.Add(tagAssemblyName);
+
+				tagProject.Elements.Add(tagPropertyGroup);
+			}
+			#endregion
+			#region PropertyGroup
+			{
+				MarkupTagElement tagPropertyGroup = new MarkupTagElement();
+				tagPropertyGroup.FullName = "PropertyGroup";
+				tagPropertyGroup.Attributes.Add("Condition", " '$(Configuration)' == 'Debug' ");
+				
+				MarkupTagElement tagIncludeDebugInformation = new MarkupTagElement();
+				tagIncludeDebugInformation.FullName = "IncludeDebugInformation";
+				tagIncludeDebugInformation.Value = "true";
+				tagPropertyGroup.Elements.Add(tagIncludeDebugInformation);
+
+				tagProject.Elements.Add(tagPropertyGroup);
+			}
+			#endregion
+			#region PropertyGroup
+			{
+				MarkupTagElement tagPropertyGroup = new MarkupTagElement();
+				tagPropertyGroup.FullName = "PropertyGroup";
+				tagPropertyGroup.Attributes.Add("Condition", " '$(Configuration)' == 'Release' ");
+
+				MarkupTagElement tagIncludeDebugInformation = new MarkupTagElement();
+				tagIncludeDebugInformation.FullName = "IncludeDebugInformation";
+				tagIncludeDebugInformation.Value = "false";
+				tagPropertyGroup.Elements.Add(tagIncludeDebugInformation);
+
+				tagProject.Elements.Add(tagPropertyGroup);
+			}
+			#endregion
+			#region ItemGroup
+			{
+				MarkupTagElement tagItemGroup = new MarkupTagElement();
+				tagItemGroup.FullName = "ItemGroup";
+				tagItemGroup.Attributes.Add("Condition", " '$(Configuration)' == 'Debug' ");
+
+				MarkupTagElement tagCompile = new MarkupTagElement();
+				tagCompile.FullName = "Compile";
+				tagCompile.Attributes.Add("Include", "filename.txt");
+				tagItemGroup.Elements.Add(tagCompile);
+
+				tagProject.Elements.Add(tagItemGroup);
+			}
+			#endregion
+
+			mom.Elements.Add(tagProject);
+			objectModels.Push(mom);
+		}
+	}
+}
---
+++ CSharp/Plugins/UniversalEditor.Essential/DataFormats/Solution/Microsoft/VisualStudio/VisualStudioSolutionDataFormat.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UniversalEditor.Accessors;
+using UniversalEditor.IO;
+using UniversalEditor.ObjectModels.PropertyList;
+using UniversalEditor.ObjectModels.Solution;
+
+namespace UniversalEditor.DataFormats.Solution.Microsoft.VisualStudio
+{
+	public class VisualStudioSolutionDataFormat : DataFormat
+	{
+		private static DataFormatReference _dfr = null;
+		public override DataFormatReference MakeReference()
+		{
+			if (_dfr == null)
+			{
+				_dfr = base.MakeReference();
+				_dfr.Capabilities.Add(typeof(SolutionObjectModel), DataFormatCapabilities.All);
+				_dfr.Filters.Add("Microsoft Visual Studio solution", new byte?[][]
+				{
+					new byte?[] { (byte)0xEF, (byte)0xBB, (byte)0xBF, (byte)0x0D, (byte)0x0A, (byte)'M', (byte)'i', (byte)'c', (byte)'r', (byte)'o', (byte)'s', (byte)'o', (byte)'f', (byte)'t', (byte)' ', (byte)'V', (byte)'i', (byte)'s', (byte)'u', (byte)'a', (byte)'l', (byte)' ', (byte)'S', (byte)'t', (byte)'u', (byte)'d', (byte)'i', (byte)'o', (byte)' ', (byte)'S', (byte)'o', (byte)'l', (byte)'u', (byte)'t', (byte)'i', (byte)'o', (byte)'n', (byte)' ', (byte)'F', (byte)'i', (byte)'l', (byte)'e', (byte)',', (byte)' ', (byte)'F', (byte)'o', (byte)'r', (byte)'m', (byte)'a', (byte)'t', (byte)' ', (byte)'V', (byte)'e', (byte)'r', (byte)'s', (byte)'i', (byte)'o', (byte)'n', (byte)' ' }
+				}, new string[] { "*.sln" }); ;
+			}
+			return _dfr;
+		}
+
+		protected override void LoadInternal(ref ObjectModel objectModel)
+		{
+			SolutionObjectModel sol = (objectModel as SolutionObjectModel);
+			if (sol == null) throw new ObjectModelNotSupportedException();
+
+			Reader reader = base.Accessor.Reader;
+			byte[] signature1 = reader.ReadBytes(3);
+			if (!signature1.Match(new byte[] { 0xEF, 0xBB, 0xBF }))
+			{
+				// this is a Unicode Byte-Order-Mark... do we have to rely on it?
+				// if we don't find it let's just back up since we may have an
+				// ASCII file even though VS is known to put the BOM in its sln
+				// files
+				reader.Accessor.Seek(-3, SeekOrigin.Current);
+			}
+
+			string signature2a = reader.ReadLine();
+			if (!String.IsNullOrEmpty(signature2a)) throw new InvalidDataFormatException("Empty line should be present at beginning of file");
+			
+			string signature2Verify = "Microsoft Visual Studio Solution File, Format Version ";
+			string signature2 = reader.ReadLine();
+			if (!signature2.StartsWith(signature2Verify)) throw new InvalidDataFormatException("File does not begin with \"" + signature2Verify + "\"");
+
+			while (!reader.EndOfStream)
+			{
+				string line = reader.ReadLine();
+				if (line.StartsWith("#") || String.IsNullOrEmpty(line)) continue;
+
+			}
+		}
+
+		protected override void SaveInternal(ObjectModel objectModel)
+		{
+			SolutionObjectModel sol = (objectModel as SolutionObjectModel);
+			if (sol == null) throw new ObjectModelNotSupportedException();
+
+			string soldir = String.Empty;
+			if (sol.Accessor is FileAccessor)
+			{
+				soldir = System.IO.Path.GetDirectoryName((sol.Accessor as FileAccessor).FileName);
+			}
+
+			Writer writer = base.Accessor.Writer;
+			writer.WriteBytes(new byte[] { 0xEF, 0xBB, 0xBF });
+			writer.WriteLine();
+			writer.WriteLine("Microsoft Visual Studio Solution File, Format Version 12.00");
+			writer.WriteLine("# Visual Studio 2012");
+
+			foreach (Project project in sol.Projects)
+			{
+				/*
+				if (project is SolutionFolder)
+				{
+					writer.WriteLine("Project(\"{2150E333-8FDC-42A3-9474-1A3956D46DE8}\") = \"" + project.Title + "\", \"" + project.Title + "\", \"" + project.ID.ToString("B") + "\"");
+					writer.WriteLine("EndProject");
+				}
+				else
+				{
+				*/
+				writer.WriteLine("Project(\"" + project.ProjectType.ID.ToString("B") + "\") = \"" + project.Title + "\", \"" + project.RelativeFileName + "\", \"" + project.ID.ToString("B") + "\"");
+				writer.WriteLine("EndProject");
+				/*
+				}
+				*/
+
+				SolutionObjectModel solproj = new SolutionObjectModel();
+				solproj.Projects.Add(project);
+				Document.Save(solproj, new VisualStudioProjectDataFormat(), new FileAccessor(soldir + "/" + project.Title + "/" + project.Title + ".ueproj"), true);
+			}
+
+			writer.WriteLine("Global");
+			writer.WriteLine("\tGlobalSection(SolutionConfigurationPlatforms) = preSolution");
+			writer.WriteLine("\t\tDebug|Any CPU = Debug|Any CPU");
+			writer.WriteLine("\t\tRelease|Any CPU = Release|Any CPU");
+			writer.WriteLine("\tEndGlobalSection");
+			writer.WriteLine("\tGlobalSection(ProjectConfigurationPlatforms) = postSolution");
+			foreach (Project project in sol.Projects)
+			{
+				writer.WriteLine("\t\t" + project.ID.ToString("B") + ".Debug|Any CPU.ActiveCfg = Debug|Any CPU");
+				writer.WriteLine("\t\t" + project.ID.ToString("B") + ".Debug|Any CPU.Build.0 = Debug|Any CPU");
+				writer.WriteLine("\t\t" + project.ID.ToString("B") + ".Debug|x86.ActiveCfg = Debug|Any CPU");
+				writer.WriteLine("\t\t" + project.ID.ToString("B") + ".Release|Any CPU.ActiveCfg = Release|Any CPU");
+				writer.WriteLine("\t\t" + project.ID.ToString("B") + ".Release|Any CPU.Build.0 = Release|Any CPU");
+				writer.WriteLine("\t\t" + project.ID.ToString("B") + ".Release|x86.ActiveCfg = Release|Any CPU");
+			}
+			writer.WriteLine("\tEndGlobalSection");
+			writer.WriteLine("\tGlobalSection(SolutionProperties) = preSolution");
+			foreach (Property prop in sol.Configuration.Properties)
+			{
+				writer.WriteLine("\t\t" + prop.Name + " = " + prop.Value);
+			}
+			writer.WriteLine("\tEndGlobalSection");
+			writer.WriteLine("\tGlobalSection(NestedProjects) = preSolution");
+			writer.WriteLine("\tEndGlobalSection");
+			writer.WriteLine("EndGlobal");
+		}
+	}
+}
