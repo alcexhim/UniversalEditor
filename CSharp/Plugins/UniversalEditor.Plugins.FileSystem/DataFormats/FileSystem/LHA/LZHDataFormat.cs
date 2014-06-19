51ce5c18fca6d3811260bcfc6a6f216f2e5f9f89
alcexhim <alcexhim@gmail.com>
2014-06-18 20:51:57 -0400

    Added LHA DataFormat (not implemented yet)

---
+++ CSharp/Plugins/UniversalEditor.Plugins.FileSystem/DataFormats/FileSystem/LHA/LZHDataFormat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniversalEditor.DataFormats.FileSystem.LHA
+{
+    public class LZHDataFormat : DataFormat
+    {
+        protected override void LoadInternal(ref ObjectModel objectModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SaveInternal(ObjectModel objectModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
