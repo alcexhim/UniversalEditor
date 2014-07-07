ae7cbebb3c255a6401822686f18c7e7f1dfc6b91
alcexhim <alcexhim@gmail.com>
2014-07-07 19:26:34 -0400

    Added file comment ability (like 7-Zip), not implemented

---
+++ CSharp/Libraries/UniversalEditor.UserInterface.WindowsForms/Dialogs/FileSystem/CommentDialog.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace UniversalEditor.Dialogs.FileSystem
+{
+	partial class CommentDialog
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtComment = new System.Windows.Forms.TextBox();
+			this.cmdCancel = new System.Windows.Forms.Button();
+			this.cmdOK = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtComment
+			// 
+			this.txtComment.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtComment.HideSelection = false;
+			this.txtComment.Location = new System.Drawing.Point(12, 12);
+			this.txtComment.Multiline = true;
+			this.txtComment.Name = "txtComment";
+			this.txtComment.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.txtComment.Size = new System.Drawing.Size(378, 142);
+			this.txtComment.TabIndex = 0;
+			// 
+			// cmdCancel
+			// 
+			this.cmdCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.cmdCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.cmdCancel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+			this.cmdCancel.Location = new System.Drawing.Point(315, 160);
+			this.cmdCancel.Name = "cmdCancel";
+			this.cmdCancel.Size = new System.Drawing.Size(75, 23);
+			this.cmdCancel.TabIndex = 1;
+			this.cmdCancel.Text = "Cancel";
+			this.cmdCancel.UseVisualStyleBackColor = true;
+			// 
+			// cmdOK
+			// 
+			this.cmdOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.cmdOK.FlatStyle = System.Windows.Forms.FlatStyle.System;
+			this.cmdOK.Location = new System.Drawing.Point(234, 160);
+			this.cmdOK.Name = "cmdOK";
+			this.cmdOK.Size = new System.Drawing.Size(75, 23);
+			this.cmdOK.TabIndex = 1;
+			this.cmdOK.Text = "&OK";
+			this.cmdOK.UseVisualStyleBackColor = true;
+			// 
+			// CommentDialog
+			// 
+			this.AcceptButton = this.cmdOK;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.cmdCancel;
+			this.ClientSize = new System.Drawing.Size(402, 195);
+			this.Controls.Add(this.cmdOK);
+			this.Controls.Add(this.cmdCancel);
+			this.Controls.Add(this.txtComment);
+			this.Name = "CommentDialog";
+			this.Text = "Comment";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button cmdCancel;
+		private System.Windows.Forms.Button cmdOK;
+		internal System.Windows.Forms.TextBox txtComment;
+	}
+}
