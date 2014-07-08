1d26099a067656773ddde02de11c38f11ec4d7f6
alcexhim <alcexhim@gmail.com>
2014-07-08 00:49:55 -0400

    Added 'Create Document' dialog to GTK interface

--- CSharp/Engines/GTK/UniversalEditor.Engines.GTK/Dialogs/CreateDocumentDialog.cs
+++ CSharp/Engines/GTK/UniversalEditor.Engines.GTK/Dialogs/CreateDocumentDialog.cs
@@ -6,7 +6,13 @@ namespace UniversalEditor.Engines.GTK.Dialogs
 		public CreateDocumentDialog ()
 		{
 			this.Build ();
-			
+			Gtk.TreeStore ts = new Gtk.TreeStore(typeof(ObjectModelReference), typeof(string), typeof(string));
+			ObjectModelReference[] omrs = UniversalEditor.Common.Reflection.GetAvailableObjectModels ();
+			foreach (ObjectModelReference omr in omrs)
+			{
+				ts.AppendValues(omr, omr.Title, omr.Description);
+			}
+			tvDocumentTypes.Model = ts;
 		}
 	}
 }
