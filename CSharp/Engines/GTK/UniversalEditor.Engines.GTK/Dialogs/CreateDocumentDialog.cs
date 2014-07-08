ccd00d9955e4cdce47e9cb36490e89d26bacdb11
alcexhim <alcexhim@gmail.com>
2014-07-08 00:44:53 -0400

    Added 'Create Document' dialog to GTK interface

---
+++ CSharp/Engines/GTK/UniversalEditor.Engines.GTK/Dialogs/CreateDocumentDialog.cs
@@ -0,0 +1,12 @@
+using System;
+namespace UniversalEditor.Engines.GTK.Dialogs
+{
+	public partial class CreateDocumentDialog : Gtk.Dialog
+	{
+		public CreateDocumentDialog ()
+		{
+			this.Build ();
+		}
+	}
+}
+
---
+++ CSharp/Engines/GTK/UniversalEditor.Engines.GTK/gtk-gui/UniversalEditor.Engines.GTK.Dialogs.CreateDocumentDialog.cs
@@ -0,0 +1,9 @@
+
+namespace UniversalEditor.Engines.GTK
+{
+	public partial class CreateDocumentDialog
+	{
+		private Gtk.Button buttonCancel;
+		private Gtk.Button buttonOk;
+	}
+}
