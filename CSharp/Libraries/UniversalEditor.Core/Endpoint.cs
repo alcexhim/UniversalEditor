850eadf27f9d0046c4bca9186533f3eec08410b2
alcexhim <alcexhim@gmail.com>
2014-07-29 19:47:34 -0400

    Added Endpoint (to be implemented in UE5)

---
+++ CSharp/Libraries/UniversalEditor.Core/Endpoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniversalEditor
+{
+	/// <summary>
+	/// Represents an endpoint (<see cref="DataFormat"/>/<see cref="Accessor"/> pair) that
+	/// defines how and where data is transferred.
+	/// </summary>
+	public class Endpoint
+	{
+		public class EndpointCollection
+			: System.Collections.ObjectModel.Collection<Endpoint>
+		{
+		}
+
+		private Accessor mvarAccessor = null;
+		public Accessor Accessor { get { return mvarAccessor; } set { mvarAccessor = value; } }
+
+		private DataFormat mvarDataFormat = null;
+		public DataFormat DataFormat { get { return mvarDataFormat; } set { mvarDataFormat = value; } }
+	}
+}
