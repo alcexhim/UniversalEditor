65c746bb3af57c27c97c4237b744258cacf41fd5
alcexhim <alcexhim@gmail.com>
2014-08-06 20:17:07 -0400

    Fixed some project files

--- CSharp/Plugins/UniversalEditor.Plugins.Microsoft/Properties/AssemblyInfo.cs
+++ CSharp/Plugins/UniversalEditor.Plugins.Microsoft/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UniversalEditor.Plugins.Microsoft")]
+[assembly: AssemblyTitle("Universal Editor plugin for Microsoft-specific data formats")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("City of Orlando")]
-[assembly: AssemblyProduct("UniversalEditor.Plugins.Microsoft")]
-[assembly: AssemblyCopyright("Copyright © City of Orlando 2014")]
+[assembly: AssemblyCompany("Mike Becker's Software")]
+[assembly: AssemblyProduct("Universal Editor Plugin Pack")]
+[assembly: AssemblyCopyright("Copyright ©2014 Mike Becker's Software")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
