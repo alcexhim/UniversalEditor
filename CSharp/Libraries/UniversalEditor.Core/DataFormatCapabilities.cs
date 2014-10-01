82d085bb6fbf3e5d1f3206c6da1dcd01952af7f0
alcexhim <alcexhim@gmail.com>
2014-09-30 20:44:18 -0400

    Refactored various DataFormat-related classes into separate files

---
+++ CSharp/Libraries/UniversalEditor.Core/DataFormatCapabilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniversalEditor
+{
+	public enum DataFormatCapabilities
+	{
+		None = 0,
+		Load = 1,
+		Save = 2,
+		Bootstrap = 4,
+		All = Load | Save | Bootstrap
+	}
+}
