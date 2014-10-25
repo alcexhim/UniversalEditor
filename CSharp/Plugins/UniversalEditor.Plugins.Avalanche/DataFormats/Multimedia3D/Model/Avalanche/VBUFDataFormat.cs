3727fa78874c86db3981f2ffc1f98c9f94c52ce0
alcexhim <alcexhim@gmail.com>
2014-10-25 10:29:27 -0400

    Tabify

--- CSharp/Plugins/UniversalEditor.Plugins.Avalanche/DataFormats/Multimedia3D/Model/Avalanche/VBUFDataFormat.cs
+++ CSharp/Plugins/UniversalEditor.Plugins.Avalanche/DataFormats/Multimedia3D/Model/Avalanche/VBUFDataFormat.cs
@@ -21,9 +21,9 @@ namespace UniversalEditor.DataFormats.Multimedia3D.Model.Avalanche
 			}
 			return _dfr;
 		}
-        
-        protected override void LoadInternal(ref ObjectModel objectModel)
-        {
+		
+		protected override void LoadInternal(ref ObjectModel objectModel)
+		{
 			ModelObjectModel model = (objectModel as ModelObjectModel);
 			if (model == null) throw new ObjectModelNotSupportedException();
 
@@ -39,41 +39,41 @@ namespace UniversalEditor.DataFormats.Multimedia3D.Model.Avalanche
 			//br.BaseStream.Seek(0, System.IO.SeekOrigin.Begin);
 			ModelSurface surf = new ModelSurface();
 
-            int count = (int)(brIBUF.Accessor.Length / 2);
+			int count = (int)(brIBUF.Accessor.Length / 2);
 			for (int i = 0; i < count; i++)
 			{
 				float vx = br.ReadSingle();
 				float vz = br.ReadSingle();
 				float vy = br.ReadSingle();
-                vx *= 100;
-                vy *= 100;
-                vz *= 100;
-                vz *= -1;
+				vx *= 100;
+				vy *= 100;
+				vz *= 100;
+				vz *= -1;
 
 				float tu = br.ReadHalf();
-                float tv = br.ReadHalf();
-                tv = 1 - tv;
+				float tv = br.ReadHalf();
+				tv = 1 - tv;
 
-                uint unknown1 = br.ReadUInt32();
-                uint vertexColor = br.ReadUInt32();
+				uint unknown1 = br.ReadUInt32();
+				uint vertexColor = br.ReadUInt32();
 
 				ModelVertex vtx = new ModelVertex(vx, vy, vz, tu, tv); // vx, vz, vy in original script??
 				surf.Vertices.Add(vtx);
 			}
-            /*
+			/*
 			for (int x = 0; x < surf.Vertices.Count; x++)
 			{
 				float nx = br.ReadSingle();
 				float ny = br.ReadSingle();
 				float nz = br.ReadSingle();
-                nz *= -1;
+				nz *= -1;
 				float un = br.ReadSingle();
-                
-                surf.Vertices[x].Normal = new PositionVector3(nx, ny, nz);
-                surf.Vertices[x].OriginalNormal = new PositionVector3(nx, ny, nz);
+				
+				surf.Vertices[x].Normal = new PositionVector3(nx, ny, nz);
+				surf.Vertices[x].OriginalNormal = new PositionVector3(nx, ny, nz);
 
-                // not sure why this needs to be here...
-                br.BaseStream.Seek(10, System.IO.SeekOrigin.Current);
+				// not sure why this needs to be here...
+				br.BaseStream.Seek(10, System.IO.SeekOrigin.Current);
 			}
 			while (!brIBUF.EndOfStream)
 			{
@@ -82,21 +82,21 @@ namespace UniversalEditor.DataFormats.Multimedia3D.Model.Avalanche
 				ushort fc = (ushort)(brIBUF.ReadUInt16() + 1);
 				surf.Triangles.Add(surf.Vertices[fa], surf.Vertices[fb], surf.Vertices[fc]);
 			}
-            */
+			*/
 
-            for (int i = 0; i < surf.Vertices.Count - 3; i += 3)
-            {
-                surf.Triangles.Add(surf.Vertices[i], surf.Vertices[i + 1], surf.Vertices[i + 2]);
-            }
+			for (int i = 0; i < surf.Vertices.Count - 3; i += 3)
+			{
+				surf.Triangles.Add(surf.Vertices[i], surf.Vertices[i + 1], surf.Vertices[i + 2]);
+			}
 
-            ModelMaterial matDefault = new ModelMaterial();
-            matDefault.Name = "default";
-            matDefault.EmissiveColor = Color.FromRGBA(255, 255, 255, 255);
-            foreach (ModelTriangle tri in surf.Triangles)
-            {
-                matDefault.Triangles.Add(tri);
-            }
-            model.Materials.Add(matDefault);
+			ModelMaterial matDefault = new ModelMaterial();
+			matDefault.Name = "default";
+			matDefault.EmissiveColor = Color.FromRGBA(255, 255, 255, 255);
+			foreach (ModelTriangle tri in surf.Triangles)
+			{
+				matDefault.Triangles.Add(tri);
+			}
+			model.Materials.Add(matDefault);
 
 			model.Surfaces.Add(surf);
 			/*
@@ -116,7 +116,7 @@ namespace UniversalEditor.DataFormats.Multimedia3D.Model.Avalanche
 			)))) else (Print "Aborted.")
 		}
 			*/
-            brIBUF.Close();
+			brIBUF.Close();
 		}
 
 		protected override void SaveInternal(ObjectModel objectModel)
