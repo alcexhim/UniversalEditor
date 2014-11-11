e1120e3b47d922920216c3bdb2e8957fbfef6dfa
alcexhim <alcexhim@gmail.com>
2014-11-11 15:27:28 -0500

    Syntactic sugar for easily implementing *Reference classes (AccessorReference, ObjectModelReference, DataFormatReference)

---
+++ CSharp/Libraries/UniversalEditor.Core/References.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniversalEditor
+{
+	public interface References<TRef>
+	{
+		TRef MakeReference();
+	}
+	public interface ReferencedBy<TObj>
+	{
+		TObj Create();
+
+		string[] GetDetails();
+		bool ShouldFilterObject(string filter);
+	}
+}
