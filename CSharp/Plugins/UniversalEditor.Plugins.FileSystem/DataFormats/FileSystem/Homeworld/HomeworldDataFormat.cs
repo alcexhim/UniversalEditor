16978e743351fac501ad729f8f0e9eec16e00f3e
alcexhim <alcexhim@gmail.com>
2014-11-27 22:55:42 -0500

    Added sanity check for header magic string

--- CSharp/Plugins/UniversalEditor.Plugins.FileSystem/DataFormats/FileSystem/Homeworld/HomeworldDataFormat.cs
+++ CSharp/Plugins/UniversalEditor.Plugins.FileSystem/DataFormats/FileSystem/Homeworld/HomeworldDataFormat.cs
@@ -6,83 +6,85 @@ using UniversalEditor.ObjectModels.FileSystem;
 
 namespace UniversalEditor.DataFormats.FileSystem.Homeworld
 {
-    public class HomeworldDataFormat : DataFormat
-    {
-        private static DataFormatReference _dfr = null;
-        public override DataFormatReference MakeReference()
-        {
-            if (_dfr == null)
-            {
-                _dfr = base.MakeReference();
-                _dfr.Capabilities.Add(typeof(FileSystemObjectModel), DataFormatCapabilities.All);
-                _dfr.Filters.Add("Homeworld VCE/WXD archive", new byte?[][] { new byte?[] { (byte)'V', (byte)'C', (byte)'E', (byte)'0' }, new byte?[] { (byte)'W', (byte)'X', (byte)'D', (byte)'1' } }, new string[] { "*.wxd", "*.vce" });
-                _dfr.ExportOptions.Add(new CustomOptionChoice("Version", "Format &version:", true,
-                    new CustomOptionFieldChoice("Version \"VCE0\"", (uint)0),
-                    new CustomOptionFieldChoice("Version \"WXD1\"", (uint)1)
-                ));
-            }
-            return _dfr;
-        }
+	public class HomeworldDataFormat : DataFormat
+	{
+		private static DataFormatReference _dfr = null;
+		public override DataFormatReference MakeReference()
+		{
+			if (_dfr == null)
+			{
+				_dfr = base.MakeReference();
+				_dfr.Capabilities.Add(typeof(FileSystemObjectModel), DataFormatCapabilities.All);
+				_dfr.Filters.Add("Homeworld VCE/WXD archive", new byte?[][] { new byte?[] { (byte)'V', (byte)'C', (byte)'E', (byte)'0' }, new byte?[] { (byte)'W', (byte)'X', (byte)'D', (byte)'1' } }, new string[] { "*.wxd", "*.vce" });
+				_dfr.ExportOptions.Add(new CustomOptionChoice("Version", "Format &version:", true,
+					new CustomOptionFieldChoice("Version \"VCE0\"", (uint)0),
+					new CustomOptionFieldChoice("Version \"WXD1\"", (uint)1)
+				));
+			}
+			return _dfr;
+		}
 
-        private uint mvarVersion = 0;
-        public uint Version { get { return mvarVersion; } set { mvarVersion = value; } }
+		private uint mvarVersion = 0;
+		public uint Version { get { return mvarVersion; } set { mvarVersion = value; } }
 
-        protected override void LoadInternal(ref ObjectModel objectModel)
-        {
-            FileSystemObjectModel fsom = (objectModel as FileSystemObjectModel);
-            if (fsom == null) return;
+		protected override void LoadInternal(ref ObjectModel objectModel)
+		{
+			FileSystemObjectModel fsom = (objectModel as FileSystemObjectModel);
+			if (fsom == null) return;
 
-            IO.Reader br = base.Accessor.Reader;
-            string header = br.ReadFixedLengthString(4);
-            uint unknown1 = br.ReadUInt32();
+			IO.Reader br = base.Accessor.Reader;
+			string header = br.ReadFixedLengthString(4);
+			if (!(header == "VCE0" || header == "WXD1")) throw new InvalidDataFormatException("File does not begin with 'VCE0' or 'WXD1'");
 
-            while (!br.EndOfStream)
-            {
-                string tag = br.ReadFixedLengthString(4);
-                switch (tag)
-                {
-                    case "INFO":
-                    {
-                        uint unknown2 = br.ReadUInt32();
-                        break;
-                    }
-                    case "DATA":
-                    {
-                        uint fileLength = br.ReadUInt32();
-                        byte[] fileData = br.ReadBytes(fileLength);
-                        fsom.Files.Add((fsom.Files.Count + 1).ToString().PadLeft(8, '0'), fileData);
-                        break;
-                    }
-                }
-            }
-        }
+			uint unknown1 = br.ReadUInt32();
 
-        protected override void SaveInternal(ObjectModel objectModel)
-        {
-            FileSystemObjectModel fsom = (objectModel as FileSystemObjectModel);
-            if (fsom == null) return;
+			while (!br.EndOfStream)
+			{
+				string tag = br.ReadFixedLengthString(4);
+				switch (tag)
+				{
+					case "INFO":
+					{
+						uint unknown2 = br.ReadUInt32();
+						break;
+					}
+					case "DATA":
+					{
+						uint fileLength = br.ReadUInt32();
+						byte[] fileData = br.ReadBytes(fileLength);
+						fsom.Files.Add((fsom.Files.Count + 1).ToString().PadLeft(8, '0'), fileData);
+						break;
+					}
+				}
+			}
+		}
 
-            IO.Writer bw = base.Accessor.Writer;
-            if (mvarVersion >= 1)
-            {
-                bw.WriteFixedLengthString("WXD1");
-            }
-            else
-            {
-                bw.WriteFixedLengthString("VCE0");
-            }
+		protected override void SaveInternal(ObjectModel objectModel)
+		{
+			FileSystemObjectModel fsom = (objectModel as FileSystemObjectModel);
+			if (fsom == null) return;
 
-            uint unknown1 = 0;
-            bw.WriteUInt32(unknown1);
+			IO.Writer bw = base.Accessor.Writer;
+			if (mvarVersion >= 1)
+			{
+				bw.WriteFixedLengthString("WXD1");
+			}
+			else
+			{
+				bw.WriteFixedLengthString("VCE0");
+			}
 
-            foreach (File file in fsom.Files)
-            {
-                bw.WriteFixedLengthString("DATA");
+			uint unknown1 = 0;
+			bw.WriteUInt32(unknown1);
 
-                byte[] data = file.GetDataAsByteArray();
-                bw.WriteUInt32((uint)data.Length);
-                bw.WriteBytes(data);
-            }
-        }
-    }
+			foreach (File file in fsom.Files)
+			{
+				bw.WriteFixedLengthString("DATA");
+
+				byte[] data = file.GetDataAsByteArray();
+				bw.WriteUInt32((uint)data.Length);
+				bw.WriteBytes(data);
+			}
+		}
+	}
 }
