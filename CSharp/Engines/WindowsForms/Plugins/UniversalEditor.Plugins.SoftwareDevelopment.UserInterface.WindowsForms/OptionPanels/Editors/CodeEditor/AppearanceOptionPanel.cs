3b1f0efc88571419fd0126edcdacf75824200dfc
alcexhim <alcexhim@gmail.com>
2014-12-10 22:20:44 -0500

    Added CodeEditor Appearance OptionPanel

---
+++ CSharp/Engines/WindowsForms/Plugins/UniversalEditor.Plugins.SoftwareDevelopment.UserInterface.WindowsForms/OptionPanels/Editors/CodeEditor/AppearanceOptionPanel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using UniversalEditor.UserInterface.WindowsForms;
+
+namespace UniversalEditor.OptionPanels.Editors.CodeEditor
+{
+	public partial class AppearanceOptionPanel : OptionPanel
+	{
+		public AppearanceOptionPanel()
+		{
+			InitializeComponent();
+		}
+
+		private string[] mvarOptionGroups = new string[] 
+		{
+			"Editors",
+			"Code Editor",
+			"Appearance"
+		};
+
+		public override string[] OptionGroups
+		{
+			get { return mvarOptionGroups; }
+		}
+	}
+}
