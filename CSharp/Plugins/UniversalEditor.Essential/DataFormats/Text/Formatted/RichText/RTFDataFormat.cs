930d683cfe11c996f15243c23e5b862f210e95bb
alcexhim <alcexhim@gmail.com>
2014-12-27 00:08:00 -0500

    Beginning to implement RTFDataFormat

---
+++ CSharp/Plugins/UniversalEditor.Essential/DataFormats/Text/Formatted/RichText/RTFDataFormat.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UniversalEditor.IO;
+using UniversalEditor.ObjectModels.Text.Formatted;
+using UniversalEditor.ObjectModels.Text.Formatted.Items;
+
+namespace UniversalEditor.DataFormats.Text.Formatted.RichText
+{
+	public class RTFDataFormat : DataFormat
+	{
+		private static DataFormatReference _dfr = null;
+		protected override DataFormatReference MakeReferenceInternal()
+		{
+			if (_dfr == null)
+			{
+				_dfr = base.MakeReferenceInternal();
+				_dfr.Capabilities.Add(typeof(FormattedTextObjectModel), DataFormatCapabilities.All);
+			}
+			return _dfr;
+		}
+
+		protected override void LoadInternal(ref ObjectModel objectModel)
+		{
+		}
+
+		protected override void SaveInternal(ObjectModel objectModel)
+		{
+			FormattedTextObjectModel ftom = (objectModel as FormattedTextObjectModel);
+			if (ftom == null) throw new ObjectModelNotSupportedException();
+
+			Writer writer = base.Accessor.Writer;
+			writer.WriteLine("{\\rtf1\\ansi\\ansicpg1252\\deff0\\deflang1033\\uc1 ");
+			foreach (FormattedTextItem item in ftom.Items)
+			{
+				RenderItem(writer, item);
+			}
+			writer.WriteLine(" }");
+		}
+
+		private void RenderItem(Writer writer, FormattedTextItem item)
+		{
+			if (item is FormattedTextItemHyperlink)
+			{
+				FormattedTextItemHyperlink itm = (item as FormattedTextItemHyperlink);
+				writer.WriteLine("{\\field{\\*\\fldinst {HYPERLINK \"" + itm.TargetURL + "\"}}{\\fldrslt {" + itm.Title + "}}}");
+			}
+		}
+	}
+}
---
+++ CSharp/Plugins/UniversalEditor.Essential/ObjectModels/Text/Formatted/Items/FormattedTextItemHyperlink.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniversalEditor.ObjectModels.Text.Formatted.Items
+{
+	public class FormattedTextItemHyperlink : FormattedTextItem
+	{
+		private string mvarTitle = String.Empty;
+		public string Title { get { return mvarTitle; } set { mvarTitle = value; } }
+
+		private string mvarTargetURL = String.Empty;
+		public string TargetURL { get { return mvarTargetURL; } set { mvarTargetURL = value; } }
+
+		public FormattedTextItemHyperlink(string targetURL = "", string title = "")
+		{
+			mvarTargetURL = targetURL;
+			if (String.IsNullOrEmpty(title))
+			{
+				mvarTitle = targetURL;
+			}
+			else
+			{
+				mvarTitle = title;
+			}
+		}
+
+		public override object Clone()
+		{
+			FormattedTextItemHyperlink clone = new FormattedTextItemHyperlink();
+			clone.TargetURL = (mvarTargetURL.Clone() as string);
+			clone.Title = (mvarTitle.Clone() as string);
+			return clone;
+		}
+	}
+}
