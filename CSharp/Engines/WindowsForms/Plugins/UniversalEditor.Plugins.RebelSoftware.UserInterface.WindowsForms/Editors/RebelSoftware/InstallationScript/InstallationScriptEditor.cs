75fa8fe5aecdbdc1f9cceba8b369356302afe668
alcexhim <alcexhim@gmail.com>
2015-04-20 15:24:41 -0400

    Improvements to Rebel Software Installer UI and implemented LoadInternal - incomplete compression

--- CSharp/Engines/WindowsForms/Plugins/UniversalEditor.Plugins.RebelSoftware.UserInterface.WindowsForms/Editors/RebelSoftware/InstallationScript/InstallationScriptEditor.cs
+++ CSharp/Engines/WindowsForms/Plugins/UniversalEditor.Plugins.RebelSoftware.UserInterface.WindowsForms/Editors/RebelSoftware/InstallationScript/InstallationScriptEditor.cs
@@ -37,46 +37,132 @@ namespace UniversalEditor.Editors.RebelSoftware.InstallationScript
 		{
 			base.OnObjectModelChanged(e);
 
-			tvExplorer.Nodes.Clear();
+			lvDialogs.Items.Clear();
 
 			InstallationScriptObjectModel script = (ObjectModel as InstallationScriptObjectModel);
 			if (script == null) return;
 
-			TreeNode nodeDialogs = new TreeNode("Dialogs");
-			nodeDialogs.ImageKey = "generic-folder-closed";
+			lvDialogs.Items.Clear();
 
 			foreach (ISDialog dialog in script.Dialogs)
 			{
-				TreeNode tn = new TreeNode();
-				if (dialog is CopyFilesDialog)
-				{
-					tn.Text = "CopyFiles";
-				}
-				else if (dialog is DestinationDialog)
-				{
-					tn.Text = "Destination";
-				}
-				else if (dialog is FinishDialog)
-				{
-					tn.Text = "Finish";
-				}
-				else if (dialog is LicenseDialog)
-				{
-					tn.Text = "License";
-				}
-				else if (dialog is StartCopyingDialog)
-				{
-					tn.Text = "StartCopying";
-				}
-				else if (dialog is WelcomeDialog)
-				{
-					tn.Text = "Welcome";
-				}
-				nodeDialogs.Nodes.Add(tn);
+				ListViewItem lviDialog = new ListViewItem();
+				lviDialog.Name = dialog.GetType().FullName;
+				lviDialog.Checked = true;
+				lviDialog.Tag = dialog;
+				lviDialog.Text = GetDialogTitle(dialog);
+				lvDialogs.Items.Add(lviDialog);
 			}
 
-			tvExplorer.Nodes.Add(nodeDialogs);
+			Type[] types = typeof(ISDialog).Assembly.GetTypes();
+			List<Type> dialogTypes = new List<Type>();
+			foreach (Type t in types)
+			{
+				if (!t.IsAbstract && t.IsSubclassOf(typeof(ISDialog)))
+				{
+					dialogTypes.Add(t);
+				}
+			}
 
+			foreach (Type t in dialogTypes)
+			{
+				if (!lvDialogs.Items.ContainsKey(t.FullName))
+				{
+					ISDialog dialog = (ISDialog)t.Assembly.CreateInstance(t.FullName);
+
+					ListViewItem lvi = new ListViewItem();
+					lvi.Name = t.FullName;
+					lvi.Text = GetDialogTitle(dialog);
+					lvi.Tag = dialog;
+					lvi.Checked = false;
+					lvDialogs.Items.Add(lvi);
+				}
+			}
+		}
+
+		private string GetDialogTitle(ISDialog dialog)
+		{
+			if (dialog is CopyFilesDialog)
+			{
+				return "Progress";
+			}
+			else if (dialog is DestinationDialog)
+			{
+				return "Destination";
+			}
+			else if (dialog is FinishDialog)
+			{
+				return "Finish";
+			}
+			else if (dialog is LicenseDialog)
+			{
+				return "License";
+			}
+			else if (dialog is StartCopyingDialog)
+			{
+				return "Summary";
+			}
+			else if (dialog is WelcomeDialog)
+			{
+				return "Welcome";
+			}
+			return dialog.GetType().FullName;
+		}
+
+		private void tvExplorer_AfterSelect(object sender, TreeViewEventArgs e)
+		{
+			foreach (Control ctl in scMain.Panel2.Controls)
+			{
+				if (e.Node != null)
+				{
+					if (ctl.Name.Substring(3) == e.Node.Name.Substring(4))
+					{
+						ctl.Enabled = true;
+						ctl.Visible = true;
+						continue;
+					}
+				}
+
+				ctl.Visible = false;
+				ctl.Enabled = false;
+			}
+		}
+
+		private void lvDialogs_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (lvDialogs.SelectedItems.Count != 1) return;
+
+			ListViewItem lvi = lvDialogs.SelectedItems[0];
+
+			picDialogPreview.Image = null;
+
+			ISDialog dialog = (lvi.Tag as ISDialog);
+			if (dialog == null) return;
+
+			if (dialog is CopyFilesDialog)
+			{
+				picDialogPreview.Image = Properties.Resources.Screenshot_Install_05_Progress;
+			}
+			else if (dialog is DestinationDialog)
+			{
+				picDialogPreview.Image = Properties.Resources.Screenshot_Install_03_Destination;
+			}
+			else if (dialog is FinishDialog)
+			{
+				picDialogPreview.Image = Properties.Resources.Screenshot_Install_06_Finish;
+			}
+			else if (dialog is LicenseDialog)
+			{
+				picDialogPreview.Image = Properties.Resources.Screenshot_Install_02_License;
+			}
+			else if (dialog is StartCopyingDialog)
+			{
+				picDialogPreview.Image = Properties.Resources.Screenshot_Install_04_Summary;
+			}
+			else if (dialog is WelcomeDialog)
+			{
+				picDialogPreview.Image = Properties.Resources.Screenshot_Install_01_Welcome;
+			}
 		}
 	}
 }
---
+++ CSharp/Plugins/UniversalEditor.Plugins.RebelSoftware/DataFormats/RebelSoftware/InstallationPackage/ZAPDataFormat.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UniversalEditor.IO;
+using UniversalEditor.ObjectModels.FileSystem;
+
+namespace UniversalEditor.DataFormats.RebelSoftware.InstallationPackage
+{
+	public class ZAPDataFormat : DataFormat
+	{
+		private static DataFormatReference _dfr = null;
+		protected override DataFormatReference MakeReferenceInternal()
+		{
+			if (_dfr == null)
+			{
+				_dfr = base.MakeReferenceInternal();
+				_dfr.Capabilities.Add(typeof(FileSystemObjectModel), DataFormatCapabilities.All);
+			}
+			return _dfr;
+		}
+
+		private uint? mvarArchiveOffset = null;
+		/// <summary>
+		/// The offset to the beginning of this archive file, useful for self-extracting executables (i.e. the length of the SFX stub executable).
+		/// </summary>
+		public uint? ArchiveOffset { get { return mvarArchiveOffset; } set { mvarArchiveOffset = value; } }
+
+		protected override void LoadInternal(ref ObjectModel objectModel)
+		{
+			FileSystemObjectModel fsom = (objectModel as FileSystemObjectModel);
+			if (fsom == null) throw new ObjectModelNotSupportedException();
+
+			Reader reader = base.Accessor.Reader;
+			if (mvarArchiveOffset != null)
+			{
+				reader.Seek(mvarArchiveOffset.Value, SeekOrigin.Begin);
+			}
+
+			uint magic = reader.ReadUInt32();
+			if (magic != 0x0C) throw new InvalidDataFormatException("File does not begin with { 0x0C, 0x00, 0x00, 0x00 }");
+
+			uint unknown1 = reader.ReadUInt32();	// 9462627
+			uint unknown2 = reader.ReadUInt32();	// 9988745
+
+			while (reader.Remaining > 4)
+			{
+				uint headerSize = reader.ReadUInt32();	// not including file name
+
+				uint fileNameLength = reader.ReadUInt32();
+				uint unknown4 = reader.ReadUInt32();
+				uint unknown5a = reader.ReadUInt32();
+				uint decompressedLength = reader.ReadUInt32();
+				
+				uint recordLength = reader.ReadUInt32(); // including header size
+
+				string fileName = reader.ReadFixedLengthString(fileNameLength);
+
+				uint footerSize = reader.ReadUInt32();
+				uint unknown7 = reader.ReadUInt32();
+				uint unknown8 = reader.ReadUInt32();
+				uint unknown9 = reader.ReadUInt32();
+
+				uint compressedLength = recordLength - footerSize;
+
+				long offset = reader.Accessor.Position;
+
+				reader.Seek(compressedLength, SeekOrigin.Current);
+
+				File file = fsom.AddFile(fileName);
+				file.Properties.Add("reader", reader);
+				file.Properties.Add("offset", offset);
+				file.Properties.Add("CompressedLength", compressedLength);
+				file.Properties.Add("DecompressedLength", decompressedLength);
+				file.DataRequest += file_DataRequest;
+				file.Size = decompressedLength;
+			}
+			
+			if (reader.Remaining == 4)
+			{
+				// offset to the beginning of the archive file
+				mvarArchiveOffset = reader.ReadUInt32();
+			}
+		}
+
+		private UniversalEditor.Compression.Modules.Zlib.ZlibCompressionModule compressor = new UniversalEditor.Compression.Modules.Zlib.ZlibCompressionModule();
+
+		private void file_DataRequest(object sender, DataRequestEventArgs e)
+		{
+			File file = (sender as File);
+			Reader reader = (Reader)file.Properties["reader"];
+			long offset = (long)file.Properties["offset"];
+			uint compressedLength = (uint)file.Properties["CompressedLength"];
+			uint decompressedLength = (uint)file.Properties["DecompressedLength"];
+
+			reader.Seek(offset, SeekOrigin.Begin);
+			byte[] compressedData = reader.ReadBytes(compressedLength);
+			byte[] decompressedData = compressor.Decompress(compressedData);
+
+			byte[] compressedDataDeflated = new byte[compressedData.Length - 6];
+			Array.Copy(compressedData, 2, compressedDataDeflated, 0, compressedDataDeflated.Length);
+
+			decompressedData = UniversalEditor.Compression.CompressionModules.Zlib.Decompress(compressedData);
+			
+			e.Data = decompressedData;
+		}
+
+		protected override void SaveInternal(ObjectModel objectModel)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
