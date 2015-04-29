9fcdb2e4c3da741c8ba29f07c18cec0bc5e29df6
alcexhim <alcexhim@gmail.com>
2015-04-29 12:26:26 -0400

    Added data format and object model for KA Actor (ATR)... I have no idea how to parse this

---
+++ CSharp/Plugins/UniversalEditor.Plugins.KnowledgeAdventure/DataFormats/KnowledgeAdventure/Actor/ATRDataFormat.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UniversalEditor.IO;
+using UniversalEditor.ObjectModels.KnowledgeAdventure.Actor;
+
+namespace UniversalEditor.DataFormats.KnowledgeAdventure.Actor
+{
+	public class ATRDataFormat : DataFormat
+	{
+		private static DataFormatReference _dfr = null;
+		protected override DataFormatReference MakeReferenceInternal()
+		{
+			if (_dfr == null)
+			{
+				_dfr = base.MakeReferenceInternal();
+				_dfr.Capabilities.Add(typeof(ActorObjectModel), DataFormatCapabilities.All);
+			}
+			return _dfr;
+		}
+
+		protected override void LoadInternal(ref ObjectModel objectModel)
+		{
+			ActorObjectModel actor = (objectModel as ActorObjectModel);
+			if (actor == null) throw new ObjectModelNotSupportedException();
+
+			Reader reader = base.Accessor.Reader;
+
+			string signature = reader.ReadFixedLengthString(5);
+			if (signature != "ATR11") throw new InvalidDataFormatException("File does not begin with 'ATR11'");
+
+			actor.Name = reader.ReadFixedLengthString(16).TrimNull();
+			actor.ImageFileName = reader.ReadFixedLengthString(32).TrimNull();
+		}
+
+		protected override void SaveInternal(ObjectModel objectModel)
+		{
+			ActorObjectModel actor = (objectModel as ActorObjectModel);
+			if (actor == null) throw new ObjectModelNotSupportedException();
+
+			Writer writer = base.Accessor.Writer;
+
+			writer.WriteFixedLengthString("ATR11");
+
+			writer.WriteFixedLengthString(actor.Name, 16);
+			writer.WriteFixedLengthString(actor.ImageFileName, 32);
+		}
+	}
+}
---
+++ CSharp/Plugins/UniversalEditor.Plugins.KnowledgeAdventure/ObjectModels/KnowledgeAdventure/Actor/ActorObjectModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniversalEditor.ObjectModels.KnowledgeAdventure.Actor
+{
+	public class ActorObjectModel : ObjectModel
+	{
+		private static ObjectModelReference _omr = null;
+		protected override ObjectModelReference MakeReferenceInternal()
+		{
+			if (_omr == null)
+			{
+				_omr = base.MakeReferenceInternal();
+				_omr.Title = "Actor";
+				_omr.Path = new string[] { "Knowledge Adventure" };
+			}
+			return _omr;
+		}
+
+		private string mvarName = String.Empty;
+		/// <summary>
+		/// The name used in scripts to identify this <see cref="Actor" />.
+		/// </summary>
+		public string Name { get { return mvarName; } set { mvarName = value; } }
+
+		private string mvarImageFileName = String.Empty;
+		/// <summary>
+		/// The file name of the image file.
+		/// </summary>
+		public string ImageFileName { get { return mvarImageFileName; } set { mvarImageFileName = value; } }
+
+		public override void Clear()
+		{
+			mvarName = String.Empty;
+			mvarImageFileName = String.Empty;
+		}
+
+		public override void CopyTo(ObjectModel where)
+		{
+			ActorObjectModel clone = (where as ActorObjectModel);
+			if (clone == null) throw new ObjectModelNotSupportedException();
+
+			clone.Name = (mvarName.Clone() as string);
+			clone.ImageFileName = (mvarImageFileName.Clone() as string);
+		}
+	}
+}
