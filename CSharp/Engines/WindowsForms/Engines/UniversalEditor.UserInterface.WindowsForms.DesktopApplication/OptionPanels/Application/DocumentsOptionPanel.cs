7b831ef37c81294523995a740032a3a96a12a468
alcexhim <alcexhim@gmail.com>
2015-05-01 00:17:39 -0400

    Improve option panels and settings storage

--- CSharp/Engines/WindowsForms/Engines/UniversalEditor.UserInterface.WindowsForms.DesktopApplication/OptionPanels/Application/DocumentsOptionPanel.cs
+++ CSharp/Engines/WindowsForms/Engines/UniversalEditor.UserInterface.WindowsForms.DesktopApplication/OptionPanels/Application/DocumentsOptionPanel.cs
@@ -5,6 +5,9 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
+using UniversalEditor.UserInterface;
+using UniversalEditor.UserInterface.Settings;
+
 namespace UniversalEditor.UserInterface.WindowsForms.OptionPanels.Application
 {
     public partial class DocumentsOptionPanel : OptionPanel
@@ -16,5 +19,22 @@ namespace UniversalEditor.UserInterface.WindowsForms.OptionPanels.Application
 
         private string[] mvarOptionGroups = new string[] { "Application", "Documents" };
         public override string[] OptionGroups { get { return mvarOptionGroups; } }
+
+		public override void LoadSettings()
+		{
+			base.LoadSettings();
+
+			string defaultUserLocation = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments) + System.IO.Path.DirectorySeparatorChar.ToString() + "Universal Editor";
+			string defaultUserDocumentsLocation = defaultUserLocation + System.IO.Path.DirectorySeparatorChar.ToString() + "Documents";
+			string defaultUserProjectsLocation = defaultUserLocation + System.IO.Path.DirectorySeparatorChar.ToString() + "Projects";
+			txtDefaultUserDocumentsLocation.Text = SettingsManager.GetPropertyValue<string>(new string[] { "Application", "Documents", "DefaultUserDocumentsLocation" }, defaultUserDocumentsLocation);
+			txtDefaultUserProjectsLocation.Text = SettingsManager.GetPropertyValue<string>(new string[] { "Application", "Documents", "DefaultUserProjectsLocation" }, defaultUserProjectsLocation);
+
+			chkAllowEditingReadonlyFiles.Checked = SettingsManager.GetPropertyValue<bool>(new string[] { "Application", "Documents", "AllowEditingReadonlyFiles" }, false);
+		}
+		public override void SaveSettings()
+		{
+			base.SaveSettings();
+		}
     }
 }
---
+++ CSharp/Libraries/UniversalEditor.UserInterface/Settings/SettingsManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniversalEditor.UserInterface.Settings
+{
+	public static class SettingsManager
+	{
+		public static T GetPropertyValue<T>(string[] path, T defaultValue = default(T))
+		{
+			return defaultValue;
+		}
+		public static void SetPropertyValue<T>(string[] path, T value)
+		{
+		}
+	}
+}
