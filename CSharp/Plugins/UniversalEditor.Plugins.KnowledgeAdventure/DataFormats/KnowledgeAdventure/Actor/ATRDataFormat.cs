a523fa737ddad5311f01ad1c19393d04f3a8c93f
alcexhim <alcexhim@gmail.com>
2015-05-03 09:39:37 -0400

    Added some more fields to KA ATR data format... still not sure what they do

--- CSharp/Plugins/UniversalEditor.Plugins.KnowledgeAdventure/DataFormats/KnowledgeAdventure/Actor/ATRDataFormat.cs
+++ CSharp/Plugins/UniversalEditor.Plugins.KnowledgeAdventure/DataFormats/KnowledgeAdventure/Actor/ATRDataFormat.cs
@@ -26,12 +26,26 @@ namespace UniversalEditor.DataFormats.KnowledgeAdventure.Actor
 			if (actor == null) throw new ObjectModelNotSupportedException();
 
 			Reader reader = base.Accessor.Reader;
-
+			
 			string signature = reader.ReadFixedLengthString(5);
 			if (signature != "ATR11") throw new InvalidDataFormatException("File does not begin with 'ATR11'");
 
 			actor.Name = reader.ReadFixedLengthString(16).TrimNull();
-			actor.ImageFileName = reader.ReadFixedLengthString(32).TrimNull();
+			actor.ImageFileName = reader.ReadFixedLengthString(16).TrimNull();
+			string referencedActorName = reader.ReadFixedLengthString(16).TrimNull();
+
+													// b0tlk.atr	beazly.atr
+			uint unknown1 = reader.ReadUInt32();	//	1900		911
+			uint unknown2 = reader.ReadUInt32();	//	0			2
+			uint unknown3 = reader.ReadUInt32();	// 34			69				width?
+			uint unknown4 = reader.ReadUInt32();	// 55			86				height?
+			uint unknown5 = reader.ReadUInt32();	// 8			8
+			uint unknown6 = reader.ReadUInt32();	// 247			1
+			uint unknown7 = reader.ReadUInt32();	// 256			316				left?
+			uint unknown8 = reader.ReadUInt32();	// 280			213				top?
+			uint unknown9 = reader.ReadUInt32();	// 306			0
+			uint unknown10 = reader.ReadUInt32();	// 0			0
+
 		}
 
 		protected override void SaveInternal(ObjectModel objectModel)
