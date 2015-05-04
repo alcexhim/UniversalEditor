c202ad453dc05d02bf78a8a5572cedeb198edcf8
alcexhim <alcexhim@gmail.com>
2015-05-04 12:46:58 -0400

    Add support for Microsoft Cabinet (MSCF) archives (untested)

---
+++ CSharp/Plugins/UniversalEditor.Plugins.Microsoft/DataFormats/FileSystem/Microsoft/Cabinet/Internal/CFFILE.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniversalEditor.DataFormats.FileSystem.Microsoft.Cabinet.Internal
+{
+    internal struct CFFILE
+    {
+        /// <summary>
+        /// uncompressed size of this file in bytes
+        /// </summary>
+        public uint decompressedSize;
+        /// <summary>
+        /// uncompressed offset of this file in the folder
+        /// </summary>
+        public uint offset;
+        /// <summary>
+        /// index into the CFFOLDER area
+        /// </summary>
+        public ushort folderIndex;
+        /// <summary>
+        /// date stamp for this file
+        /// </summary>
+        public ushort date;
+        /// <summary>
+        /// time stamp for this file
+        /// </summary>
+        public ushort time;
+        /// <summary>
+        /// attribute flags for this file
+        /// </summary>
+        public ushort attribs;
+        /// <summary>
+        /// name of this file
+        /// </summary>
+        public string name;
+    }
+}
---
+++ CSharp/Plugins/UniversalEditor.Plugins.Microsoft/DataFormats/FileSystem/Microsoft/Cabinet/Internal/CFFOLDER.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniversalEditor.DataFormats.FileSystem.Microsoft.Cabinet.Internal
+{
+    internal struct CFFOLDER
+    {
+        /// <summary>
+        /// offset of the first CFDATA block in this folder
+        /// </summary>
+        public uint firstDataBlockOffset;
+        /// <summary>
+        /// number of CFDATA blocks in this folder
+        /// </summary>
+        public ushort dataBlockCount;
+        /// <summary>
+        /// compression type indicator
+        /// </summary>
+        public CABCompressionMethod compressionMethod;
+        /// <summary>
+        /// (optional) per-folder reserved area
+        /// </summary>
+        public byte[] reservedArea;
+    }
+}
