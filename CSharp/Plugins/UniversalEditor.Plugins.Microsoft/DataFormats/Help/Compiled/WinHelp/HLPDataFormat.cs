3a9f30812a0f8a524fd1dd999909db52064818e2
alcexhim <alcexhim@gmail.com>
2015-11-30 11:23:59 -0500

    Start to work on WinHelp-specific part of BPlusFileSystem

---
+++ CSharp/Plugins/UniversalEditor.Plugins.Microsoft/DataFormats/Help/Compiled/WinHelp/HLPDataFormat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UniversalEditor.DataFormats.FileSystem.BPlus;
+using UniversalEditor.ObjectModels.FileSystem;
+
+using UniversalEditor.ObjectModels.Help.Compiled;
+
+namespace UniversalEditor.DataFormats.Help.Compiled.WinHelp
+{
+	/// <summary>
+	/// The Windows Help (WinHelp) HLP data format. Much of the framework for reading these files is contained in BPlusFileSystemDataFormat, since the container file format
+	/// itself is remarkably generic (albeit unused, as far as I can tell, apart from WinHelp files), but WinHelp-specific stuff is here.
+	/// </summary>
+	public class HLPDataFormat : BPlusFileSystemDataFormat
+	{
+		protected override void BeforeLoadInternal(Stack<ObjectModel> objectModels)
+		{
+			base.BeforeLoadInternal(objectModels);
+			objectModels.Push(new FileSystemObjectModel());
+		}
+		protected override void AfterLoadInternal(Stack<ObjectModel> objectModels)
+		{
+			base.AfterLoadInternal(objectModels);
+			
+			FileSystemObjectModel fsom = (objectModels.Pop() as FileSystemObjectModel);
+			CompiledHelpObjectModel help = (objectModels.Pop() as CompiledHelpObjectModel);
+		}
+		protected override void BeforeSaveInternal(Stack<ObjectModel> objectModels)
+		{
+			base.BeforeSaveInternal(objectModels);
+
+			CompiledHelpObjectModel help = (objectModels.Pop() as CompiledHelpObjectModel);
+			FileSystemObjectModel fsom = new FileSystemObjectModel();
+
+			objectModels.Push(fsom);
+		}
+	}
+}
---
+++ CSharp/Plugins/UniversalEditor.Plugins.Microsoft/ObjectModels/Help/Compiled/CompiledHelpObjectModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniversalEditor.ObjectModels.Help.Compiled
+{
+	public class CompiledHelpObjectModel : ObjectModel
+	{
+		public override void Clear()
+		{
+		}
+
+		public override void CopyTo(ObjectModel where)
+		{
+		}
+	}
+}
