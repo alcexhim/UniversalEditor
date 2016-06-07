ac3ba46ff2a5fc202ff0d149e98f9c404e6d9050
Michael Becker <alcexhim@gmail.com>
2016-06-07 01:12:54 -0400

    Added constructor for CodeCommentElement

--- CSharp/Plugins/UniversalEditor.Plugins.SoftwareDevelopment/ObjectModels/SourceCode/CodeElements/CodeCommentElement.cs
+++ CSharp/Plugins/UniversalEditor.Plugins.SoftwareDevelopment/ObjectModels/SourceCode/CodeElements/CodeCommentElement.cs
@@ -7,6 +7,14 @@ namespace UniversalEditor.ObjectModels.SourceCode.CodeElements
 {
     public class CodeCommentElement : CodeElement
     {
+		public CodeCommentElement(string content = null, bool multiline = false, bool isDocumentationComment = false)
+		{
+			if (content == null) content = String.Empty;
+			mvarContent = content;
+			mvarMultiline = multiline;
+			mvarIsDocumentationComment = isDocumentationComment;
+		}
+
         private string mvarContent = String.Empty;
         public string Content { get { return mvarContent; } set { mvarContent = value; } }
 
