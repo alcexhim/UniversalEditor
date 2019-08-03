76b05293336d73cbcab16ea63036f314f2f56f8b
Michael Becker <alcexhim@gmail.com>
2019-08-03 11:16:16 -0400

    Also copy the .config and .pdb files to the appropriate locations

--- CSharp/uwt-install.sh
+++ CSharp/uwt-install.sh
@@ -1,3 +1,5 @@
 cd ../../UniversalWidgetToolkit
 cp Output/Debug/UniversalWidgetToolkit.Engines.GTK.dll ../UniversalEditor/CSharp/Output/Debug
+cp Output/Debug/UniversalWidgetToolkit.Engines.GTK.dll.config ../UniversalEditor/CSharp/Output/Debug
+cp Output/Debug/UniversalWidgetToolkit.Engines.GTK.pdb ../UniversalEditor/CSharp/Output/Debug
 
