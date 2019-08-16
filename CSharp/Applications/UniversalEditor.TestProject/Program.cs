7e2e98ce3535102c37b7840bd40b73f654d67c39
Michael Becker <alcexhim@gmail.com>
2019-08-16 01:07:22 -0400

    Add test project for bench-testing whatever we want without loading the entire UE UI

---
+++ CSharp/Applications/UniversalEditor.TestProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UniversalEditor.Accessors;
+using UniversalEditor.DataFormats.Package.OpenPackagingConvention;
+using UniversalEditor.ObjectModels.Package;
+
+namespace UniversalEditor.TestProject
+{
+	class MainClass
+	{
+		public static void Main (string [] args)
+		{
+			string fileName = "/tmp/UETest/test.opc";
+
+			PackageObjectModel om = new PackageObjectModel ();
+			OPCDataFormat df = new OPCDataFormat ();
+
+			Document.Save (om, df, new FileAccessor (fileName, true, true));
+		}
+	}
+}
