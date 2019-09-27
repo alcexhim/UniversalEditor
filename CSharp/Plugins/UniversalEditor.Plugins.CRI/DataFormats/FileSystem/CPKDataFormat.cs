d820dc087af54d546d9622986c242ea000141699
Michael Becker <alcexhim@gmail.com>
2019-09-27 19:37:52 -0400

    Add CRI plugin (for AFS, CPK, and other data formats)

---
+++ CSharp/Plugins/UniversalEditor.Plugins.CRI/DataFormats/FileSystem/CPKDataFormat.cs
@@ -0,0 +1,36 @@
+﻿//
+//  CPKDataFormat.cs
+//
+//  Author:
+//       Mike Becker <alcexhim@gmail.com>
+//
+//  Copyright (c) 2019 Mike Becker
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+namespace UniversalEditor.Plugins.CRI.DataFormats.FileSystem
+{
+	public class CPKDataFormat : DataFormat
+	{
+		protected override void LoadInternal(ref ObjectModel objectModel)
+		{
+
+		}
+
+		protected override void SaveInternal(ObjectModel objectModel)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
