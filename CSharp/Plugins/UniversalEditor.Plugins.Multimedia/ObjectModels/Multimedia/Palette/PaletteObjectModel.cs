fe6b9af042eb2ad609fe0899caa6d35eac000a88
Michael Becker <alcexhim@gmail.com>
2019-10-13 02:40:12 -0400

    Add Name property to PaletteObjectModel

--- CSharp/Plugins/UniversalEditor.Plugins.Multimedia/ObjectModels/Multimedia/Palette/PaletteObjectModel.cs
+++ CSharp/Plugins/UniversalEditor.Plugins.Multimedia/ObjectModels/Multimedia/Palette/PaletteObjectModel.cs
@@ -5,35 +5,37 @@ using System.Text;
 
 namespace UniversalEditor.ObjectModels.Multimedia.Palette
 {
-    public class PaletteObjectModel : ObjectModel
-    {
-        private static ObjectModelReference _omr = null;
-        protected override ObjectModelReference MakeReferenceInternal()
-        {
-            if (_omr == null)
-            {
-                _omr = base.MakeReferenceInternal();
-                _omr.Title = "Color palette";
-                _omr.Path = new string[] { "Multimedia", "Color palette" };
-            }
-            return _omr;
-        }
+	public class PaletteObjectModel : ObjectModel
+	{
+		private static ObjectModelReference _omr = null;
+		protected override ObjectModelReference MakeReferenceInternal()
+		{
+			if (_omr == null)
+			{
+				_omr = base.MakeReferenceInternal();
+				_omr.Title = "Color palette";
+				_omr.Path = new string[] { "Multimedia", "Color palette" };
+			}
+			return _omr;
+		}
 
-        public override void Clear()
-        {
-            mvarEntries.Clear();
-        }
+		public override void Clear()
+		{
+			mvarEntries.Clear();
+		}
 
-        public override void CopyTo(ObjectModel where)
-        {
-            PaletteObjectModel clone = (where as PaletteObjectModel);
-            foreach (PaletteEntry entry in mvarEntries)
-            {
-                clone.Entries.Add(entry.Clone() as PaletteEntry);
-            }
-        }
+		public override void CopyTo(ObjectModel where)
+		{
+			PaletteObjectModel clone = (where as PaletteObjectModel);
+			foreach (PaletteEntry entry in mvarEntries)
+			{
+				clone.Entries.Add(entry.Clone() as PaletteEntry);
+			}
+		}
 
-        private PaletteEntry.PaletteEntryCollection mvarEntries = new PaletteEntry.PaletteEntryCollection();
-        public PaletteEntry.PaletteEntryCollection Entries { get { return mvarEntries; } }
-    }
+		public string Name { get; set; } = String.Empty;
+
+		private PaletteEntry.PaletteEntryCollection mvarEntries = new PaletteEntry.PaletteEntryCollection();
+		public PaletteEntry.PaletteEntryCollection Entries { get { return mvarEntries; } }
+	}
 }
