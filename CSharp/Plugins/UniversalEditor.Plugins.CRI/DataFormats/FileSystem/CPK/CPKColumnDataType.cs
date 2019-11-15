8f797ee03f3ab044b4e7fb53c614038f03c55cc9
Michael Becker <alcexhim@gmail.com>
2019-11-14 23:08:50 -0500

    the CPK data format should be in a separate CRI plugin after all...

--- CSharp/Plugins/UniversalEditor.Plugins.CRI/DataFormats/FileSystem/CPK/CPKColumnDataType.cs
+++ CSharp/Plugins/UniversalEditor.Plugins.CRI/DataFormats/FileSystem/CPK/CPKColumnDataType.cs
@@ -19,7 +19,7 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System;
-namespace UniversalEditor.DataFormats.FileSystem.SEGA.CPK
+namespace UniversalEditor.Plugins.CRI.DataFormats.FileSystem.CPK
 {
 	enum CPKColumnDataType : byte
 	{
