0a75cc0a3c95cdce33c15df38fb8f003779ac36e
Michael Becker <alcexhim@gmail.com>
2019-11-30 03:48:33 -0500

    preliminary implementation (load filenames only) of playlist editor

---
+++ CSharp/Plugins.UserInterface/UniversalEditor.Plugins.Multimedia.UserInterface/Editors/Multimedia/Playlist/PlaylistEditor.cs
@@ -0,0 +1,87 @@
+﻿//
+//  PlaylistEditor.cs
+//
+//  Author:
+//       Mike Becker <alcexhim@gmail.com>
+//
+//  Copyright (c) 2019 Mike Becker
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using MBS.Framework.UserInterface;
+using UniversalEditor.ObjectModels.Multimedia.Playlist;
+using UniversalEditor.UserInterface;
+
+namespace UniversalEditor.Plugins.Multimedia.UserInterface.Editors.Multimedia.Playlist
+{
+	public partial class PlaylistEditor
+	{
+		public PlaylistEditor()
+		{
+			InitializeComponent();
+
+			// fuckkkk
+			for (int i = 0; i < tmPlaylist.Columns.Count; i++)
+			{
+				lvPlaylist.Columns[i].Column = tmPlaylist.Columns[i];
+			}
+		}
+
+		private static EditorReference _er = null;
+		public override EditorReference MakeReference()
+		{
+			if (_er == null)
+			{
+				_er = base.MakeReference();
+				_er.SupportedObjectModels.Add(typeof(PlaylistObjectModel));
+			}
+			return _er;
+		}
+
+		public override void UpdateSelections()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override EditorSelection CreateSelectionInternal(object content)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void OnObjectModelChanged(EventArgs e)
+		{
+			base.OnObjectModelChanged(e);
+
+			tmPlaylist.Rows.Clear();
+
+			PlaylistObjectModel playlist = (ObjectModel as PlaylistObjectModel);
+			if (playlist == null) return;
+
+			for (int i = 0; i < playlist.Entries.Count; i++)
+			{
+				TreeModelRow row = new TreeModelRow();
+				row.RowColumns.Add(new TreeModelRowColumn(tmPlaylist.Columns[0], playlist.Entries[i].FileName));
+				row.SetExtraData<PlaylistEntry>("entry", playlist.Entries[i]);
+
+				tmPlaylist.Rows.Add(row);
+			}
+		}
+
+		protected override void OnCreated(EventArgs e)
+		{
+			base.OnCreated(e);
+			lvPlaylist.Model = tmPlaylist;
+		}
+	}
+}
