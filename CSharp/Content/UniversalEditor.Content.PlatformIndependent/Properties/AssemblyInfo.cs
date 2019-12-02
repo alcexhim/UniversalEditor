328b6d55e3fa30aad5d95edb68f1f6a3eb65d6c3
Michael Becker <alcexhim@gmail.com>
2019-12-02 08:05:34 -0500

    fix assembly info

--- CSharp/Content/UniversalEditor.Content.PlatformIndependent/Properties/AssemblyInfo.cs
+++ CSharp/Content/UniversalEditor.Content.PlatformIndependent/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UniversalEditor.Content.PlatformIndependent")]
+[assembly: AssemblyTitle("Universal Editor Platform-Independent Content")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("City of Orlando")]
-[assembly: AssemblyProduct("UniversalEditor.Content.PlatformIndependent")]
-[assembly: AssemblyCopyright("Copyright © City of Orlando 2014")]
+[assembly: AssemblyCompany("Mike Becker's Software")]
+[assembly: AssemblyProduct("Universal Editor")]
+[assembly: AssemblyCopyright("Copyright © 2014-2019 Mike Becker's Software")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
