bb0f3bcf26634289cafd0826384788b631f99118
Michael Becker <alcexhim@gmail.com>
2020-05-04 02:09:10 -0400

    gzipstream must be CLOSED before it will flush data to the underlying stream ; is this a problem also with DeflateStream?

--- Libraries/UniversalEditor.Compression/Modules/Gzip/GzipCompressionModule.cs
+++ Libraries/UniversalEditor.Compression/Modules/Gzip/GzipCompressionModule.cs
@@ -12,34 +12,29 @@ namespace UniversalEditor.Compression.Modules.Gzip
 			get { return "gzip"; }
 		}
 
-        public const int BUFFERSIZE = 4096;
+		public const int BUFFERSIZE = 4096;
 
 		protected override void CompressInternal(System.IO.Stream inputStream, System.IO.Stream outputStream)
-        {
-            System.IO.Compression.GZipStream dst = new System.IO.Compression.GZipStream(outputStream, System.IO.Compression.CompressionMode.Compress);
-            int read = 0;
-            int start = 0;
-            do
-            {
-                byte[] buffer = new byte[BUFFERSIZE];
-                read = inputStream.Read(buffer, start, buffer.Length);
-                dst.Write(buffer, 0, read);
-            }
-            while (read == BUFFERSIZE);
+		{
+			System.IO.Compression.GZipStream dst = new System.IO.Compression.GZipStream(outputStream, System.IO.Compression.CompressionMode.Compress, true);
+			inputStream.CopyTo(dst);
+
+			// this is crazy. we have to pass a special parameter to keep the stream open, and explicitly call Close on the stream..... for it to work properly???
+			dst.Close();
 		}
 
 		protected override void DecompressInternal(System.IO.Stream inputStream, System.IO.Stream outputStream, int inputLength, int outputLength)
-        {
-            System.IO.Compression.GZipStream dst = new System.IO.Compression.GZipStream(inputStream, System.IO.Compression.CompressionMode.Decompress);
-            int read = 0;
-            int start = 0;
-            do
-            {
-                byte[] buffer = new byte[BUFFERSIZE];
-                read = dst.Read(buffer, start, buffer.Length);
-                outputStream.Write(buffer, 0, read);
-            }
-            while (read == BUFFERSIZE);
+		{
+			System.IO.Compression.GZipStream dst = new System.IO.Compression.GZipStream(inputStream, System.IO.Compression.CompressionMode.Decompress);
+			int read = 0;
+			int start = 0;
+			do
+			{
+				byte[] buffer = new byte[BUFFERSIZE];
+				read = dst.Read(buffer, start, buffer.Length);
+				outputStream.Write(buffer, 0, read);
+			}
+			while (read == BUFFERSIZE);
 		}
 	}
 }
