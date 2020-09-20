1375a418f9fdde904e9a326c28f6f0957e9015a3
Michael Becker <alcexhim@gmail.com>
2020-09-20 01:13:11 -0400

    preliminary SLOOOW plugin for reading blockchain databases

---
+++ Plugins/UniversalEditor.Plugins.Blockchain/Bitcoin/ObjectModels/BitcoinBlockTransaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace UniversalEditor.Plugins.Blockchain.Bitcoin.ObjectModels
+{
+	public class BitcoinBlockTransaction : BlockTransaction
+	{
+		public byte[] PublicKeyScript { get; set; }
+
+		public class BitcoinBlockTransactionCollection
+			: System.Collections.ObjectModel.Collection<BitcoinBlockTransaction>
+		{
+
+		}
+
+		public override object Clone()
+		{
+			BitcoinBlockTransaction clone = new BitcoinBlockTransaction();
+			return clone;
+		}
+	}
+}
---
+++ Plugins/UniversalEditor.Plugins.Blockchain/Bitcoin/ObjectModels/BitcoinBlockchainObjectModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace UniversalEditor.Plugins.Blockchain.Bitcoin.ObjectModels
+{
+	public class BitcoinBlockchainObjectModel : ObjectModel
+	{
+		private static ObjectModelReference _omr = null;
+		protected override ObjectModelReference MakeReferenceInternal()
+		{
+			if (_omr == null)
+			{
+				_omr = base.MakeReferenceInternal();
+				_omr.Path = new string[] { "Blockchain", "Bitcoin" };
+			}
+			return _omr;
+		}
+
+		public BitcoinBlock.BitcoinBlockCollection Blocks { get; } = new BitcoinBlock.BitcoinBlockCollection();
+
+		public override void Clear()
+		{
+			Blocks.Clear();
+		}
+
+		public override void CopyTo(ObjectModel where)
+		{
+			BitcoinBlockchainObjectModel clone = (where as BitcoinBlockchainObjectModel);
+			if (clone == null)
+				throw new ObjectModelNotSupportedException();
+
+			for (int i = 0; i < Blocks.Count;i++)
+			{
+				clone.Blocks.Add(Blocks[i].Clone() as BitcoinBlock);
+			}
+		}
+	}
+}
