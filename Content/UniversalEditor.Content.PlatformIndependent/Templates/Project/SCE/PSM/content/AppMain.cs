0a917c6c7fc89d3701228ad393f1bf080ccab5fa
Michael Becker <alcexhim@gmail.com>
2020-10-20 20:59:14 -0400

    added SCE PlayStation Mobile basic project template

---
+++ Content/UniversalEditor.Content.PlatformIndependent/Templates/Project/SCE/PSM/content/AppMain.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+
+using Sce.PlayStation.Core;
+using Sce.PlayStation.Core.Environment;
+using Sce.PlayStation.Core.Graphics;
+using Sce.PlayStation.Core.Input;
+
+namespace $(Project.Title)
+{
+	public class AppMain
+	{
+		private static GraphicsContext graphics;
+		
+		public static void Main (string[] args)
+		{
+			Initialize ();
+
+			while (true) {
+				SystemEvents.CheckEvents ();
+				Update ();
+				Render ();
+			}
+		}
+
+		public static void Initialize ()
+		{
+			// Set up the graphics system
+			graphics = new GraphicsContext ();
+		}
+
+		public static void Update ()
+		{
+			// Query gamepad for current state
+			var gamePadData = GamePad.GetData (0);
+		}
+
+		public static void Render ()
+		{
+			// Clear the screen
+			graphics.SetClearColor (0.0f, 0.0f, 0.0f, 0.0f);
+			graphics.Clear ();
+
+			// Present the screen
+			graphics.SwapBuffers ();
+		}
+	}
+}
