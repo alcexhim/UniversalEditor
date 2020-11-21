41136a6a1058a1bc3d745a3de751d5d96c5bc420
Michael Becker <alcexhim@gmail.com>
2020-11-20 22:17:41 -0500

    start to move some common stuff out of UE and into MBS.Framework.UserInterface

--- Libraries/UniversalEditor.UserInterface/Dialogs/GenericBrowserPopup.cs
+++ Libraries/UniversalEditor.UserInterface/Dialogs/GenericBrowserPopup.cs
@@ -25,55 +25,22 @@ using System.Collections.ObjectModel;
 using MBS.Framework.UserInterface;
 using MBS.Framework.UserInterface.Controls;
 using MBS.Framework.UserInterface.Controls.ListView;
+using MBS.Framework.UserInterface.Dialogs;
 using MBS.Framework.UserInterface.Input.Keyboard;
 
 namespace UniversalEditor.UserInterface.Dialogs
 {
-	public partial class GenericBrowserPopup<TObj, TRef> : CustomDialog
+	public class GenericBrowserPopup<TObj, TRef> : SearchableDropdownListDialog
 		where TObj : class, References<TRef>
 		where TRef : class, ReferencedBy<TObj>
 	{
-		public GenericBrowserPopup()
-		{
-			this.InitializeComponent();
-		}
-
-		public event EventHandler SelectionChanged;
-
-		private bool mvarAutoClose = true;
-		public bool AutoClose { get { return mvarAutoClose; } set { mvarAutoClose = value; } }
-		/*
-		protected override void OnDeactivate(EventArgs e)
-		{
-			base.OnDeactivate(e);
-			if (mvarAutoClose) this.Close();
-		}
-		*/
 		public Collection<TRef> AvailableObjects { get; } = new Collection<TRef>();
 		public TObj SelectedObject { get; set; } = default(TObj);
 
-		/*
-		protected override void OnShown(EventArgs e)
+		protected override void UpdateSearchInternal(string query)
 		{
-			base.OnShown(e);
-			UpdateSearch();
-		}
-		*/
+			base.UpdateSearchInternal(query);
 
-		protected override void OnCreated(EventArgs e)
-		{
-			base.OnCreated(e);
-			UpdateSearch();
-		}
-
-		private void txtSearch_Changed(object sender, EventArgs e)
-		{
-			UpdateSearch();
-		}
-
-		private void UpdateSearch()
-		{
-			tm.Rows.Clear();
 			foreach (TRef item in AvailableObjects)
 			{
 				bool itemShouldFilter = false;
@@ -81,89 +48,48 @@ namespace UniversalEditor.UserInterface.Dialogs
 				foreach (string detail in details)
 				{
 					if (detail == null) continue;
-					if (detail.ToLower().Trim().Contains(txtSearch.Text.ToLower().Trim()))
+					if (detail.ToLower().Trim().Contains(query.ToLower().Trim()))
 					{
 						itemShouldFilter = true;
 						break;
 					}
 				}
-				if (String.IsNullOrEmpty(txtSearch.Text.Trim()) || itemShouldFilter)
+				if (String.IsNullOrEmpty(query.Trim()) || itemShouldFilter)
 				{
 					AddObjectToList(item);
 				}
 			}
-
-			if (tm.Rows.Count == 1)
-			{
-				lv.SelectedRows.Clear();
-				lv.SelectedRows.Add(tm.Rows[0]);
-			}
-			// lv.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
 		}
 
 		private void AddObjectToList(TRef itmr)
 		{
 			string[] details = itmr.GetDetails();
 			List<TreeModelRowColumn> columns = new List<TreeModelRowColumn>();
-			
+
 			for (int i = 0; i < details.Length; i++)
 			{
 				string str = details[i];
 				if (String.IsNullOrEmpty(str)) str = String.Empty;
-				
-				columns.Add(new TreeModelRowColumn(tm.Columns[i], str));
+
+				columns.Add(CreateColumn(i, str));
 			}
 
-			TreeModelRow lvi = new TreeModelRow(columns.ToArray()); 
+			TreeModelRow lvi = new TreeModelRow(columns.ToArray());
 			lvi.SetExtraData<TRef>("TRef", itmr);
-			tm.Rows.Add(lvi);
+			AddRow(lvi);
 		}
 
-		private void lv_RowActivated(object sender, ListViewRowActivatedEventArgs e)
+		protected override void SelectRow(TreeModelRow row)
 		{
-			// if (lv.SelectedItems.Count != 1) return;
-
-			SelectedObject = e.Row.GetExtraData<TRef>("TRef")?.Create();
-			SelectionChanged?.Invoke(this, e);
-
-			DialogResult = DialogResult.OK;
-			Close();
-		}
-
-		private void txtSearch_KeyDown(object sender, KeyEventArgs e)
-		{
-			if (e.Key == KeyboardKey.Enter)
+			base.SelectRow(row);
+			if (row == null)
 			{
-				e.Cancel = true;
-				if (lv.SelectedRows.Count != 1) return;
-				
-				SelectedObject = lv.SelectedRows[0].GetExtraData<TRef>("TRef")?.Create();
-				if (SelectionChanged != null) SelectionChanged(this, e);
-				
-				Close();
+				SelectedObject = null;
 			}
-			else if (e.Key == KeyboardKey.Escape)
+			else
 			{
-				e.Cancel = true;
-
-				// already handled by GTK? but what about other platforms
-				Close();
+				SelectedObject = row.GetExtraData<TRef>("TRef")?.Create();
 			}
-		}
-
-		public event EventHandler ResetList;
-
-		private void cmdReset_Click(object sender, EventArgs e)
-		{
-			ResetList?.Invoke(this, e);
-			UpdateSearch();
-		}
-
-		private void cmdNone_Click(object sender, EventArgs e)
-		{
-			SelectedObject = null;
-			SelectionChanged?.Invoke(this, e);
-			Close();
 		}
 	}
 }
