a26763560ac08a6b5be9f637edb333e6e4af813c
Michael Becker <alcexhim@gmail.com>
2021-01-09 22:44:22 -0500

    explicitly declare CABFlags to be ushort

--- Plugins/UniversalEditor.Plugins.Microsoft/DataFormats/FileSystem/Microsoft/Cabinet/CABFlags.cs
+++ Plugins/UniversalEditor.Plugins.Microsoft/DataFormats/FileSystem/Microsoft/Cabinet/CABFlags.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace UniversalEditor.DataFormats.FileSystem.Microsoft.Cabinet
 {
     [Flags()]
-    public enum CABFlags
+    public enum CABFlags : ushort
     {
         HasPreviousCabinet = 0x0001,
         HasNextCabinet = 0x0002,
