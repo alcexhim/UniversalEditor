5e757970e9cfb958080513f210bab66bffd08c35
Michael Becker <alcexhim@gmail.com>
2021-05-05 00:34:47 -0400

    we already prompt the user with whiptail, so assume -y

--- firstrun.sh
+++ firstrun.sh
@@ -39,7 +39,7 @@ then
 	MONO_SUBFLAVOR="main"
 
 	echo "installing prerequisites"
-	sudo apt install gnupg ca-certificates
+	sudo apt install -y gnupg ca-certificates
 
 	echo "receiving public key"
 	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
@@ -48,7 +48,7 @@ then
 	echo "deb https://download.mono-project.com/repo/$MONO_REPO $MONO_FLAVOR $MONO_SUBFLAVOR" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 	sudo apt update
 
-	sudo apt install mono-complete
+	sudo apt install -y mono-complete
 fi
 
 # ================ CLONE DEPENDENT PROJECTS FROM AUTHORS REPOSITORY ================
