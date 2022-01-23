6c3d7de2936a2406898dc5ae10dd89725b2351a2
Michael Becker <alcexhim@gmail.com>
2022-01-23 15:07:52 -0500

    initial commit of upgrade to .NET 4.8

--- Plugins.UserInterface/UniversalEditor.Plugins.Executable.UserInterface/Editors/Executable/NonLayout/ManagedAssemblyPanel.cs
+++ Plugins.UserInterface/UniversalEditor.Plugins.Executable.UserInterface/Editors/Executable/NonLayout/ManagedAssemblyPanel.cs
@@ -228,6 +228,10 @@ namespace UniversalEditor.Plugins.Executable.UserInterface.Editors.Executable
 
 			for (int i = 0; i < codeProviders.Length; i++)
 			{
+				// FIXME: this gets called in WinForms but not GTK engine??
+				if (codeProviders[i].IsAbstract)
+					continue;
+
 				CodeProvider codeProvider = (codeProviders[i].Assembly.CreateInstance(codeProviders[i].FullName) as CodeProvider);
 				TreeModelRow row = new TreeModelRow(new TreeModelRowColumn[]
 				{
