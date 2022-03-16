307d71334aed248e6f3d9c86ecb05a0e308b6202
Michael Becker <alcexhim@gmail.com>
2022-03-16 12:40:59 -0400

    fix install and launch scripts

--- install.sh
+++ install.sh
@@ -3,7 +3,10 @@
 cp MainIcon.png /usr/share/icons/universal-editor.png
 cp net.alcetech.UniversalEditor.desktop /usr/share/applications
 
-mkdir /usr/lib/universal-editor
+if [ ! -d /usr/lib/universal-editor ]; then
+	mkdir /usr/lib/universal-editor
+fi
+
 cp -r Output/Debug/* /usr/lib/universal-editor
 
 cp universal-editor /usr/lib/universal-editor
@@ -11,7 +14,7 @@ cp universal-editor /usr/lib/universal-editor
 echo "#!/bin/sh
 
 cd /usr/lib/universal-editor
-./universal-editor ${1+\"$@\"}
+./universal-editor \${1+\"\$@\"}
 " > /usr/bin/universal-editor
 
 chmod a+x /usr/bin/universal-editor
