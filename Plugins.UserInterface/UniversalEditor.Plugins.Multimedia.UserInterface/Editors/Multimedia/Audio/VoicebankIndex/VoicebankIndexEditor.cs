abe872cd3ea51cccf5abf8c5e4acbc40a6896ba5
Michael Becker <alcexhim@gmail.com>
2023-02-17 17:45:17 -0500

    add VoicebankIndex editor

---
+++ Plugins.UserInterface/UniversalEditor.Plugins.Multimedia.UserInterface/Editors/Multimedia/Audio/VoicebankIndex/VoicebankIndexEditor.cs
@@ -0,0 +1,33 @@
+//
+//  VoicebankIndexEditor.cs
+//
+//  Author:
+//       Michael Becker <alcexhim@gmail.com>
+//
+//  Copyright (c) 2022 Mike Becker's Software
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+namespace UniversalEditor.Plugins.Multimedia.UserInterface.Editors.Multimedia.Audio.VoicebankIndex
+{
+	/// <summary>
+	/// Provides a UWT-based <see cref="Editor" /> for a <see cref="VoicebankObjectModel" />.
+	/// </summary>
+	public class VoicebankIndexEditor
+	{
+		public VoicebankIndexEditor()
+		{
+		}
+	}
+}
